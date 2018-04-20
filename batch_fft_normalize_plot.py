6f684491026bdbfc939dcf2589046dc1534c2985
deostroll <deostroll@gmail.com>
2018-04-21 00:58:49 +0530

    plotting the normalized curves in batch

--- batch_fft_normalize_plot.py
+++ batch_fft_normalize_plot.py
@@ -23,6 +23,11 @@ def write_data_to_csv(file, data):
 
 def get_normed_data(data):
 	x , y  = data
+
+	filtered = np.array([ (a,b) for a, b, c in zip(x, y, y/np.max(y))  if c > 0.5 ])
+	x = filtered[:, 0]
+	y = filtered[:, 1]
+
 	return (x - np.min(x), y - np.min(y))
 
 
@@ -38,10 +43,14 @@ def main(compare_file):
 	
 	def plot(b, image):
 		ln.set_data(b[0], b[1])
+		# plt.ylim(np.max(inormed[1], b[1]))
+		plt.title(image)
 		plt.savefig(image)
 
 	s = 'norm_'
 	
+	normed_list = []
+
 	for x in os.listdir('processed'):
 		actual_file_path = os.path.join('processed', x)
 		file_name = os.path.basename(actual_file_path)
@@ -51,8 +60,18 @@ def main(compare_file):
 			_, x, y = get_data_from_csv(actual_file_path)
 			normed = (x, y)
 			image_file = 'processed/fig_norm_compare_%s.png' % (base_name[len(s):])
-			plot(normed, image_file)
-			print("Saved:", image_file)
+			normed_list.append((image_file, normed))
+			# plot(normed, image_file)
+			# print("Saved:", image_file)
+
+	y_list = list(inormed[0])
+	for _, normed_data in normed_list:
+		y_list = y_list + normed_data[1]
+
+	plt.ylim((0, np.max(y_list)))
+	for image_file, normed_data in normed_list:
+		plot(normed_data, image_file)
+		print('Saved:', image_file)
 
 if __name__ == '__main__':
 	main(sys.argv[1])
